ed702251eaa1d63c165b0f7c5463405e4132d34f
KhanLewis <khanlewis@hotmail.co.uk>
2023-04-18 09:24:23 +0000

    added alert messages to view and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -80,6 +80,8 @@ TEMPLATES = [
     },
 ]
 
+MESSAGEL_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
